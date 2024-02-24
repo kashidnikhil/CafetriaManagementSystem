d93c86718f5013554f4f0c7c4cc7c91ab5d54ce7
unknown <chaitanya@tigmo.com.au>
2024-02-24 23:07:21 +0530

    Changed category to counter

--- sidebar_cust.php
+++ sidebar_cust.php
@@ -1,7 +1,7 @@
             <div class="col-lg-3" id="sidebar-menu">
                 <a href="emphome.php">Dashboard</a>
                 <a href="items-list.php">View Items</a>
-                <a href="food-category-list.php">View Category</a>
+                <a href="food-category-list.php">View Counters</a>
                 <a href="emordstat.php">Pending Orders</a>
                 <a href="emordview.php">Completed Orders</a>
                 <a href="empprof.php">Profile</a>
